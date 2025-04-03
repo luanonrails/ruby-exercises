c733d27e71a43b6627e3ced1243be39115c1f536
Luan Ferreira <181789192+luanonrails@users.noreply.github.com>
2025-04-03 10:07:45 -0300

    Adicionar 04_bubble_sort

---
+++ 04_bubble_sort/bubble_sort.rb
@@ -0,0 +1,10 @@
+def bubble_sort(array)
+  for i in 0...array.length - 1
+    for j in i + 1...array.length
+      if array[i] > array[j]
+        array[i], array[j] = array[j], array[i]
+      end
+    end
+  end
+  return array
+end
